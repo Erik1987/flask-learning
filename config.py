c1077586c580ca4e05b78f300920903c9486e81d
Erik <you@example.com>
2022-03-31 13:10:31 +0300

    kehitys-8h

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
-
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
@@ -10,9 +11,9 @@ class Config:
         ['true', 'on', '1']
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
-    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    Projekti_MAIL_SUBJECT_PREFIX = '[Projekti]'
+    Projekti_MAIL_SENDER = 'Projekti Admin <Projekti@example.com>'
+    Projekti_ADMIN = os.environ.get('FP_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     @staticmethod
